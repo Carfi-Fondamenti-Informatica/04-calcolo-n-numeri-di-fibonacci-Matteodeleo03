ace481e126ec7831cb3dad8e32cac1ed7472ced8
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-11-24 23:09:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   int n, i, primo = 1, secondo = 1, terzo;
+    cin >> n;
+
+    if (n < 2) {
+        cout << "errore";
+    } else {
+        cout << primo << "," << secondo;
+
+        for (i = 2; i < n; i++) {
+            terzo = primo + secondo;
+            primo = secondo;
+            secondo = terzo;
+            cout << "," << terzo;
+        }
+    }
+}
    return 0;
 }
