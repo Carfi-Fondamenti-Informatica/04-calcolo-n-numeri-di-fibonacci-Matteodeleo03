f295faf233a2b993828c82c60dd7e20fa62f00c7
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-11-24 23:32:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,20 @@
 using namespace std;
 
 int main() {
-   int n, i, primo = 1, secondo = 1, terzo;
+  int n, i, primo = 1, secondo = 1, terzo;
     cin >> n;
 
     if (n < 2) {
         cout << "errore";
     } else {
-        cout << primo << "," << secondo;
+        cout << primo << endl;
+        cout << secondo << endl;
 
         for (i = 2; i < n; i++) {
             terzo = primo + secondo;
             primo = secondo;
             secondo = terzo;
-            cout << "," << terzo;
+            cout << terzo << endl;
         }
     }
 }
